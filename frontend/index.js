c1e28f9b3ff7de61d27464cd088e94e7df6a114d
soojinpark99 <154590790+soojinpark99@users.noreply.github.com>
2024-01-08 17:16:48 +0900

    ✨ input 값의 유효성을 체크하는 기능 구현
    - input 값이 비어있거나 word list에 알파벳 이외의 문자를 입력한 경우 안내 메시지를 띄우는 기능 구현

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,9 @@
 const form = document.getElementById("maker-form");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  window.location.href = "/game.html";
-});
-
 // input 자동완성 해제
 document.querySelectorAll("input").forEach(function (input) {
   input.setAttribute("autocomplete", "off");
 });
-
-async function handleSubmitForm(event) {
-  event.preventDefault();
-
-  // formdata가 서버에 업로드되는 로직 구현하기
-}
 
 // Enter 눌렀을 때 Submit 되는 걸 방지
 document.addEventListener(
@@ -27,4 +16,56 @@ document.addEventListener(
   true
 );
 
-form.addEventListener("submit", handleSubmitForm);
+// title, description input 값 유효성 체크
+function validateInput(event, input, err) {
+  event.preventDefault();
+
+  const inputField = document.querySelector(input);
+  const errBox = document.querySelector(err);
+
+  if (inputField.value.trim() == "") {
+    errBox.style.display = "flex";
+  } else {
+    errBox.style.display = "none";
+  }
+}
+
+// word list input 값 유효성 체크
+function validateWordInput(event) {
+  event.preventDefault();
+
+  const wordInputs = document.querySelectorAll(".word-input");
+  const wordErr = document.querySelector(".word.input-err");
+  const engErr = document.querySelector(".eng.input-err");
+
+  // 비어있는 word input field 및 영어가 아닌 값 확인
+  let filledCount = 0;
+  for (let i = 0; i < wordInputs.length; i++) {
+    const inputValue = wordInputs[i].value.trim();
+
+    if (inputValue !== "") {
+      filledCount++;
+
+      if (!/^[a-zA-Z]+$/.test(inputValue)) {
+        engErr.style.display = "flex";
+        return false; // 제출 차단
+      } else {
+        engErr.style.display = "none";
+      }
+    }
+  }
+
+  if (filledCount < 10) {
+    wordErr.style.display = "flex";
+  } else {
+    wordErr.style.display = "none";
+  }
+}
+
+form.addEventListener("submit", function (event) {
+  validateInput(event, ".title-input", ".title.input-err");
+  validateInput(event, ".description-input", ".description.input-err");
+  validateWordInput(event);
+});
+
+// formdata가 서버에 업로드되는 로직 구현하기
